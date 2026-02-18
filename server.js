eb40c65bf07f357fac7606f686192ba1015e3a5a
user <user@users-MacBook-Pro.local>
2026-02-18 20:22:31 +0000

    Update CORS for production

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://rebellohealth-frontend.vercel.app"],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/auth", require("./routes/authRoutes"));
 
